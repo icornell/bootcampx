e2259aed419db0ea70688975379bf6d4725c8849
India Cornell <indiaccornell@gmail.com>
2023-09-03 22:14:42 -0700

    Add more queries

---
+++ 4_queries/7_average_assistance_request_wait_time.sql
@@ -0,0 +1,6 @@
+--Average Assistance Request Wait Time
+--Calculate the average time it takes to start an assistance request.
+--Return just a single column here
+
+SELECT avg(started_at-created_at) as average_wait_time
+FROM assistance_requests;
