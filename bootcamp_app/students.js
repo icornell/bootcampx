1d70bcaff317ca1939990bb49167a8430b4f25f6
India Cornell <indiaccornell@gmail.com>
2023-09-04 19:49:46 -0700

    update for bootcamp_app and connecting node postgres

---
+++ bootcamp_app/students.js
@@ -0,0 +1,51 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "vagrant",
+  password: "123",
+  host: "localhost",
+  database: "bootcampx",
+});
+
+
+const cohortName = process.argv[2] || ""; // Get the cohort name from the command line arguments
+const limit = process.argv[3] || 5; // Get the limit from the command line arguments; default to 5 if not provided
+// Use parameterized queries to avoid SQL injection
+const queryString = `
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+FROM students
+JOIN cohorts ON cohorts.id = students.cohort_id
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+
+const values = [`%${cohortName}%`, limit];
+
+pool
+  .query(queryString, values)
+  .then((res) => {
+    res.rows.forEach((user) => {
+      console.log(
+        `${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`
+      );
+    });
+  })
+  .catch((err) => console.error("query error", err.stack));
+
+  /*
+answer from the solution:
+
+pool.query(`
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+FROM students
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE '%${process.argv[2]}%'
+LIMIT ${process.argv[3] || 5};
+`)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+  })
+}).catch(err => console.error('query error', err.stack));
+
+  */
